6d0b114b92ba0d99a961b3608e69fd6edd3ff96a
Filipe de Souza Martins <filipe.martins@arcelormittal.com.br>
2024-01-31 13:31:26 -0300

    REFACT: Improving settings.py file

--- settings/settings.py
+++ settings/settings.py
@@ -1,9 +1,18 @@
 from os import environ, path
-
+from pathlib import Path
 from dotenv import load_dotenv
 
 env_file = path.join(path.dirname(__file__), '.env')
-load_dotenv(env_file)
 
+if path.exists(env_file):
+    load_dotenv(env_file)
+else:
+    raise FileNotFoundError(
+        'File settings/.env was not found. Please implement and configure your .env file.'
+    )
+
+base_dir = Path(__file__).resolve().parent.parent
 
 name = environ.get('NAME')
+
+usuario = environ.get('username')
